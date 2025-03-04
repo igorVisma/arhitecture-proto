44d60a158fcae337dae55965aa593326c4d3e389
Igor Kornilov <igor.kornilov@visma.com>
2025-03-04 15:21:43 +0200

    feat(apps): integrate SharedComponent from @repo/ui and update dependencies

--- apps/root/src/routes/index.tsx
+++ apps/root/src/routes/index.tsx
@@ -1,14 +1,16 @@
-import * as React from 'react'
-import { createFileRoute } from '@tanstack/react-router'
+import * as React from "react";
+import { createFileRoute } from "@tanstack/react-router";
+import { SharedComponent } from "@repo/ui/shared-component";
 
-export const Route = createFileRoute('/')({
-  component: HomeComponent,
-})
+export const Route = createFileRoute("/")({
+	component: HomeComponent,
+});
 
 function HomeComponent() {
-  return (
-    <div className="p-2">
-      <h3>Welcome Home!</h3>
-    </div>
-  )
+	return (
+		<div className="p-2">
+			<h3>Welcome Home!</h3>
+			<SharedComponent />
+		</div>
+	);
 }
---
+++ packages/ui/components/shared-component.tsx
@@ -0,0 +1,15 @@
+export const SharedComponent = () => {
+	return (
+		<div
+			style={{
+				backgroundColor: "lightblue",
+				padding: "3rem",
+				width: "300px",
+				textAlign: "center",
+				color: "black",
+				fontSize: "2.5rem",
+			}}>
+			Shared Component
+		</div>
+	);
+};
